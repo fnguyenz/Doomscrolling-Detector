58b5a0ce34deab26fc9c90f62972fb17a8fc5fe9
fae <faen769@deltalearns.ca>
2026-03-05 10:28:38 -0800

    Plan out code through commenting
    I don't have a full idea on how to do this, so I commented out a very general structure to help me learn as I go

---
+++ doomscrolling-detector.py
@@ -0,0 +1,19 @@
+''' 
+DOOM SCROLLING DETECTOR!!!
+THIS IS BEING MADE TO CREATE SOME SORT OF ALARM AND DISPLAY WHEN I AM OFF TASK
+(OR ON MY PHONE IN SPECIFIC)
+MAKE USE OF THE WEBCAM, 
+POSSIBLY MAKE A TIMER TO DETERMINE HOW LONG I AM ACTUALLY WORKING VS OFF TASK
+'''
+
+# import any modules required
+
+
+# state variables
+
+
+# create functions for detecting, activating, and anything else needed while making this
+
+
+# use gui to make this look more organized and easy to navigate
+
